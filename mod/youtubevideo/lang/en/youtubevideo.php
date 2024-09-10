346fb6764cca489b7e422ad76c14d177bc998735
Karim Kompissi <karimkompissi@gmail.com>
2024-09-10 14:42:36 +0100

    add new lang string

--- mod/youtubevideo/lang/en/youtubevideo.php
+++ mod/youtubevideo/lang/en/youtubevideo.php
@@ -16,3 +16,11 @@ $string['pluginname'] = 'YouTube Video';
 $string['youtubeurl'] = 'YouTube URL';
 $string['youtubeurl_help'] = 'Enter the URL of the YouTube video you want to embed.';
 $string['invalidyoutubeurl'] = 'The entered YouTube URL is not valid.';
+
+$string['videoslist'] = 'List of YouTube videos';
+$string['delete'] = 'Delete';
+$string['invalidyoutubeurl'] = 'Invalid YouTube URL';
+$string['savechanges'] = 'Save changes';
+$string['changessaved'] = 'Changes saved successfully';
+$string['manageyoutubevideos'] = 'Manage YouTube videos';
+$string['youtubevideo:manage'] = 'Manage YouTube videos';

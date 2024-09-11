a968e36a67baa2cb78e2d158597a44b20bba7be2
Karim Kompissi <karimkompissi@gmail.com>
2024-09-11 09:47:26 +0100

    fix manage youtube video

--- mod/youtubevideo/lang/en/youtubevideo.php
+++ mod/youtubevideo/lang/en/youtubevideo.php
@@ -24,4 +24,11 @@ $string['savechanges'] = 'Save changes';
 $string['changessaved'] = 'Changes saved successfully';
 $string['manageyoutubevideos'] = 'Manage YouTube videos';
 $string['youtubevideo:manage'] = 'Manage YouTube videos';
+$string['novideos'] = 'No YouTube videos have been added to this course yet.';
+$string['activitydeleted'] = 'This YouTube video activity has been deleted. You will be redirected to the course page.';
+$string['eventvideoupdated'] = 'YouTube video updated';
+$string['eventvideodeleted'] = 'YouTube video deleted';
 $string['pluginadministration'] = 'YouTube Video Administration';
+
+// https://www.youtube.com/watch?v=yyUHQIec83I
+
--- mod/youtubevideo/manage.php
+++ mod/youtubevideo/manage.php
@@ -2,17 +2,21 @@
 
 /**
  * Manage Youtube Video
- * 
- * @package youtubevideo
- * @copyright 2024 NYX-EI {@link https://nyx-ei.tech}
- * @author NYX-EI <help@nyx-ei.tech>
+ *
+ * @package    mod_youtubevideo
+ * @copyright  2024 NYX-EI {@link https://nyx-ei.tech}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once(__DIR__ . '/../../config.php');
-require_once($CFG->dirroot.'/mod/youtubevideo/lib.php');
-require_once($CFG->dirroot . '/course/moodleform_mod.php');
+require_once($CFG->dirroot . '/mod/youtubevideo/lib.php');
+require_once($CFG->libdir . '/formslib.php');
 
-$id = required_param('id', PARAM_INT);
+$id = optional_param('id', 0, PARAM_INT); // Course ID
+
+if (!$id) {
+    print_error('missingcourseid', 'youtubevideo');
+}
 
 $course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
 
@@ -20,29 +24,35 @@ require_login($course);
 $context = context_course::instance($course->id);
 require_capability('mod/youtubevideo:manage', $context);
 
-$PAGE->set_url('/mod/youtubevideo/manage.php', array('id' => $id));
+$PAGE->set_url('/mod/youtubevideo/manage.php', array('id' => $course->id));
 $PAGE->set_title(format_string($course->fullname));
 $PAGE->set_heading(format_string($course->fullname));
 $PAGE->set_pagelayout('incourse');
 
 class mod_youtubevideo_manage_form extends moodleform
 {
-
-    public function definition()
+    protected function definition()
     {
         global $DB, $COURSE;
 
         $mform = $this->_form;
-
         $videos = $DB->get_records('youtubevideo', array('course' => $COURSE->id));
 
         $mform->addElement('header', 'videoslist', get_string('videoslist', 'youtubevideo'));
 
-        foreach ($videos as $video) {
-            $mform->addElement('text', 'video_' . $video->id, $video->name, array('size' => '60'));
-            $mform->setType('video_' . $video->id, PARAM_URL);
-            $mform->addElement('checkbox', 'delete_' . $video->id, get_string('delete'));
+        if (empty($videos)) {
+            $mform->addElement('static', 'novideos', '', get_string('novideos', 'youtubevideo'));
+        } else {
+            foreach ($videos as $video) {
+                $mform->addElement('text', 'video_' . $video->id, $video->name, array('size' => '60'));
+                $mform->setType('video_' . $video->id, PARAM_URL);
+                $mform->setDefault('video_' . $video->id, $video->youtubeurl);
+                $mform->addElement('checkbox', 'delete_' . $video->id, get_string('delete'));
+            }
         }
+
+        $mform->addElement('hidden', 'id', $COURSE->id);
+        $mform->setType('id', PARAM_INT);
 
         $this->add_action_buttons(true, get_string('savechanges'));
     }
@@ -63,17 +73,34 @@ class mod_youtubevideo_manage_form extends moodleform
     }
 }
 
-function youtubevideo_process_changes($data)
+/**
+ * Process changes to YouTube videos
+ *
+ * @param object $data Form data
+ * @param int $courseid Course ID
+ */
+function youtubevideo_process_changes($data, $courseid)
 {
-    global $DB, $COURSE;
+    global $DB;
 
     foreach ($data as $key => $value) {
         if (strpos($key, 'video_') === 0) {
             $id = substr($key, 6);
-            $DB->set_field('youtubevideo', 'intro', $value, array('id' => $id, 'course' => $COURSE->id));
+            $video = $DB->get_record('youtubevideo', array('id' => $id, 'course' => $courseid));
+
+            if ($video) {
+                if ($video->youtubeurl !== $value) {
+                    // URL has been modified
+                    $DB->update_record('youtubevideo', (object) array(
+                        'id' => $id,
+                        'youtubeurl' => $value,
+                        'timemodified' => time()
+                    ));
+                }
+            }
         } elseif (strpos($key, 'delete_') === 0 && $value == 1) {
             $id = substr($key, 7);
-            $DB->delete_records('youtubevideo', array('id' => $id, 'course' => $COURSE->id));
+            $DB->delete_records('youtubevideo', array('id' => $id, 'course' => $courseid));
         }
     }
 }
@@ -81,11 +108,11 @@ function youtubevideo_process_changes($data)
 $mform = new mod_youtubevideo_manage_form();
 
 if ($mform->is_cancelled()) {
-    redirect(new moodle_url('/course/view.php', array('id' => $COURSE->id)));
+    redirect(new moodle_url('/course/view.php', array('id' => $course->id)));
 } else if ($fromform = $mform->get_data()) {
-    youtubevideo_process_changes($fromform);
+    youtubevideo_process_changes($fromform, $course->id);
     redirect(
-        new moodle_url('/course/view.php', array('id' => $COURSE->id)),
+        new moodle_url('/mod/youtubevideo/manage.php', array('id' => $course->id)),
         get_string('changessaved', 'youtubevideo'),
         null,
         \core\output\notification::NOTIFY_SUCCESS

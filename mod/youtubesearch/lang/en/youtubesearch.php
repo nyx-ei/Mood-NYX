09efef207b51191c69752bf3a9d0b4a4a812b57b
Karim Kompissi <karimkompissi@gmail.com>
2024-09-20 05:33:36 +0100

    add: add lang file for plugin

---
+++ mod/youtubesearch/lang/en/youtubesearch.php
@@ -0,0 +1,12 @@
+<?php
+$string['modulename'] = 'YouTube Search';
+$string['modulenameplural'] = 'YouTube Searches';
+$string['pluginname'] = 'YouTube Search';
+$string['api_key'] = 'YouTube API Key';
+$string['api_key_desc'] = 'Enter your YouTube Data API v3 key here';
+$string['search_placeholder'] = 'Search YouTube videos';
+$string['search_button'] = 'Search';
+$string['preview_button'] = 'Preview';
+$string['add_to_course'] = 'Add to course';
+$string['video_added_success'] = 'Video added successfully';
+$string['video_add_failed'] = 'Failed to add video';

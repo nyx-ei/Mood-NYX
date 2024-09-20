dff1b36beb2e7c48cc867ed7008636bd29b4970c
Karim Kompissi <karimkompissi@gmail.com>
2024-09-20 05:43:17 +0100

    create: create templates to display the search results

--- mod/youtubesearch/version.php
+++ mod/youtubesearch/version.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @package youtubesearch
+ * @copyright 2024 NYX-EI {@link https://nyx-ei.tech}
+ * @author NYX-EI <help@nyx-ei.tech>
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2024091900;
